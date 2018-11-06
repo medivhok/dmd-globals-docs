86fa59fd00e63aceac68f39a8b7c46590f8cf103
Jean Gregory Verret <gregory.verret@intelia.com>
2018-11-06 12:10:41 -0500

    plugin is now working

--- helpers/global-link.js
+++ helpers/global-link.js
@@ -1,16 +1,5 @@
 const gd = require('globals-docs');
 
 
-exports.globallink = (longname, options) => {
-  let result = null;
-
-  if (longname && typeof longname === 'string') {
-    const url = gd.getDoc(longname);
-
-    if (url) {
-        result = options.fn({ name: longname, url });
-    }
-  }
-
-  return result;
-};
+exports.globallink = ({ name, url, ...rest }, options) =>
+  options.fn({ name, url: (name && !url) ? gd.getDoc(name) : url, ...rest });
